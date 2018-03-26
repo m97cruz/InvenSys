fd6334d5cfa28ad14468a20a0e4b4dc176126012
mcruz <mcruz@MCruzPC>
2018-03-25 19:54:09 -0600

    Clase de conexion para la base de datos (el usuario y contraseña puede variar... usando XAMPP)

---
+++ src/DBControl/Conexion.java
@@ -0,0 +1,26 @@
+
+package DBControl;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author mcruz
+ */
+public class Conexion {
+    public Connection conn(){
+        String dbURL = "jdbc:mysql://localhost:3306/ferreteria_torres";
+        String usr = "root";
+        String pass= "";
+        Connection conn = null;
+        
+        try{
+            conn = (Connection) DriverManager.getConnection(dbURL, usr, pass);
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
+}
